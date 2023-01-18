e0fd6dfa6c3ef4db7f5d3319257e531148d8056e
Alex12k <i-gps@yandex.ru>
2023-01-18 15:11:10 +0600

      Конец 42 урока Ошибка белый экран

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,7 +2,22 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+
+let initialState =	{
+	profilePage: {
+		posts: [
+			 { id: 1, message: 'Hi, how are you?', likeCount: 12 },
+			 { id: 2, message: 'It\'s my first post', likeCount: 11 },
+			 { id: 3, message: 'BlaBla', likeCount: 3 },
+			 { id: 4, message: 'Da da', likeCount: 174 },
+		],
+
+		newPostText: 'it-kamasutra.com'
+
+  }
+}
+
+const profileReducer = (state = initialState, action) => {
 	
 	switch(action.type){			
 			case ADD_POST:						
