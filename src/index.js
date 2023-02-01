07501e6e31bf44732a9f2178ff1b09a3a2ddb143
Alex12k <i-gps@yandex.ru>
2023-02-02 02:26:20 +0600

    Перед внедрением StoreContext

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import StoreContext from './StoreContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
     root.render(
         <BrowserRouter>
-            <App state={state}  dispatch={store.dispatch.bind(store)}   store={store}/>
+            <StoreContext.Provider value={store}>
+                <App state={state}  dispatch={store.dispatch.bind(store)}   store={store}/>
+            </StoreContext.Provider>
         </BrowserRouter>
     );
 }
